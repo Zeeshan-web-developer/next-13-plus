37f88f9890eaf9fcde702f3df8b42a8e575821f1
Zeeshan Ahmad <zeeshan>
2023-04-11 19:48:04 +0530

    catch all optional dynamic parameter

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Nfound() {
+  return (
+    <div>404 Not Found</div>
+  )
+}
+
+export default Nfound
---
+++ app/projects/[[...pid]]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>catch all with optional page</div>
+  )
+}
+
+export default page
