e5a910d5e3ebac53123a4ca1aabcac3146d42b15
Zeeshan Ahmad <zeeshan>
2023-04-11 10:24:25 +0530

    creating a dynamic route inside user/order/[orderId] to fetch order info by id

---
+++ app/(dashboard)/profile/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Route group profile page</div>
+  )
+}
+
+export default page
---
+++ app/(dashboard)/shop/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Route group shop page</div>
+  )
+}
+
+export default page
